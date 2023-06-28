e0a3d7cae07ee2824ec9d52c082d35467b6ba1ff
Bryan Fleming <bryanfleming@gmail.com>
2023-06-28 15:19:01 -0500

    Sync system config files for Bryan

---
+++ commit_configs.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+cd `dirname $0`
+
+# make sure .zprezto exists
+mkdir -p ./.zprezto/runcoms
+
+# grab brew information
+`which brew` leaves > ./my-casks.txt
+
+# grab zsh configs for zprezto
+cp -R $HOME/.zprezto/runcoms/z* .zprezto/runcoms/
+
+# grab vimrc config
+cp $HOME/.vimrc .
+
+# complete the git calls to commit code
+git add --all
+git commit -m "Sync system config files for Bryan"
+git push origin main
---
+++ sync-configs.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+cd `dirname $0`
+
+PID=`$(which pidof) fswatch`
+FILELIST="$HOME.vimrc $HOME/.zprezto"
+
+if [ -n $PID ] ; then
+    exit 1;
+fi
+
+`which fswatch` -o $FILELIST | xargs -n1 -I{} ./commit_configs.sh &
