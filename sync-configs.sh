55d0c740b99ffb81629a2c84aaf2d23ac6597b44
Bryan Fleming <bryanfleming@gmail.com>
2023-07-03 16:47:17 -0500

    Sync system config files for Bryan #17

--- sync-configs.sh
+++ sync-configs.sh
@@ -3,7 +3,6 @@
 cd `dirname $0`
 
 PID=`/usr/local/bin/pidof fswatch`
-NOHUPCMD='/usr/bin/nohup'
 FSWATCHCMD='/usr/local/bin/fswatch'
 FILELIST="$HOME/.vimrc $HOME/.zprezto/runcoms"
 
@@ -11,5 +10,12 @@ if [ -n "$PID" ] ; then
     exit 1;
 fi
 
-(${NOHUPCMD} ${FSWATCHCMD} -0 -Lr -o ${FILELIST} | xargs -0 -n1 ./commit_configs.sh) &
+SESSION="fswatch"
+PANE="${session}.0"
+TMUXCMD=`which tmux`
 
+if [[ -z "$(${TMUXCMD} ls | grep fswatch)" ]]; then
+    $TMUXCMD new -d -s ${PANE}
+fi
+${TMUXCMD} send-keys -t ${PANE} C-z "${FSWATCHCMD} -0 -Lr -o ${FILELIST} | xargs -0 -n1 ./commit_configs.sh" Enter
+#(${FSWATCHCMD} -0 -Lr -o ${FILELIST} | xargs -0 -n1 ./commit_configs.sh) &
