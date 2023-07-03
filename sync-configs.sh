853595895be0e99a04aebded5856f8c7b65fe390
Bryan Fleming <bryanfleming@gmail.com>
2023-07-03 13:47:29 -0500

    Sync system config files for Bryan #13

--- sync-configs.sh
+++ sync-configs.sh
@@ -3,10 +3,11 @@
 cd `dirname $0`
 
 PID=`/usr/local/bin/pidof fswatch`
-FILELIST="$HOME.vimrc $HOME/.zprezto/runcoms"
+FSWATCHCMD='/usr/local/bin/fswatch'
+FILELIST="$HOME/.vimrc $HOME/.zprezto/runcoms"
 
 if [ -n "$PID" ] ; then
     exit 1;
 fi
 
-(/usr/local/bin/fswatch -r -o $FILELIST | xargs -n1 -I{} ./commit_configs.sh) &
+(${FSWATCHCMD} -Lr -o ${FILELIST} | xargs -n1 -I{} ./commit_configs.sh) &
