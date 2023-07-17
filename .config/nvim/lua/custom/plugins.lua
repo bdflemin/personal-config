0511f8fe803da73760999b1673d66e201c7f6d9d
Bryan Fleming <bryanfleming@gmail.com>
2023-07-17 14:46:23 -0400

    Sync system config files for Bryan #42

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,5 +1,12 @@
 local plugins = {
   {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
