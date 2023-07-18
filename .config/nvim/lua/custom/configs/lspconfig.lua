57505665591087fed104c311b9641e9360873a10
Bryan Fleming <bryanfleming@gmail.com>
2023-07-18 17:26:21 -0400

    Sync system config files for Bryan #79

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -10,3 +10,10 @@ lspconfig.pyright.setup({
   capabilities = capabilities,
   filetypes = {"python"},
 })
+
+lspconfig.ansiblels.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"yaml"},
+})
+
